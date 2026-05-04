a156bdc00f137ccceafe7c3c7ae4e7a79c5298a3
DebanshiSwain <debanshiswain11@gmail.com>
2026-05-04 21:53:04 +0530

    admins.sql

---
+++ database/admins.sql
@@ -0,0 +1,8 @@
+CREATE TABLE admins (
+    username VARCHAR(50),
+    password VARCHAR(50)
+);
+
+INSERT INTO admins VALUES ('simran','sim123');
+INSERT INTO admins VALUES ('debanshi','deb123');
+INSERT INTO admins VALUES ('narayani','nar123');
